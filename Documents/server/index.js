45d5054bcf94836b69841594fbc37c4ee9809046
Tai Nguyen <nguyenductai.job@gmail.com>
2021-07-04 17:31:13 +0700

    init

---
+++ Documents/server/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send({hi: 'hi tai'});
+});
+const PORT = process.env.PORT || 5000;
+app.listen(5000);
